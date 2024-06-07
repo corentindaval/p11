91c6a0552b730f5f186c2dd110483fe984cc3f12
corentindaval <60287459+corentindaval@users.noreply.github.com>
2024-06-07 09:35:26 +0200

    mise en page responsive

--- src/components/a_propos.jsx
+++ src/components/a_propos.jsx
@@ -6,7 +6,7 @@ function A_propos() {
         <div id="composant_a_propos">
             <img id="img_fond2" src={img_fond2}></img>
             <div id="partie_a_propos">
-             <div id="Fiabilité">
+             <div id="Fiabilite">
                 <Collapse description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." titre="fiabilité" liste={false} nom_compo="txt_fiabilite" nom_contenu="contenu_fiabilite" />
              </div>
              <div id="Respect">
@@ -15,7 +15,7 @@ function A_propos() {
              <div id="Service">
                 <Collapse description="La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme." titre="service" liste={false} nom_compo="txt_service" nom_contenu="contenu_service" />
              </div>
-             <div id="Sécurité">
+             <div id="Securite">
                 <Collapse description="La sécurité est la priorité de Kasa. Aussi bien pour nos hotes que pour les voyageurs,chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hote qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hotes." titre="sécurité" liste={false} nom_compo="txt_securite" nom_contenu="contenu_securite" />
              </div>
             </div>
--- src/components/carrousel_image.jsx
+++ src/components/carrousel_image.jsx
@@ -2,45 +2,50 @@
 
 export default function Carrousel_image(props) {
     const { liste_image } = props;
-    console.log("list:" + liste_image);
-    console.log(props)
-    const aff_img = document.getElementById("img_actuel");
-    const aff_num_img = document.getElementById("num_img");
+    const [donnee_num, setDonnee_num] = useState(1 + "/" + liste_image.length);
+    const [aff_img, setAff_img] = useState(liste_image[0])
+    const [index, setIndex] = useState(0)
+
+    //console.log("list:" + liste_image);
+    //console.log(props)
+
     var liste = [];
     liste = liste_image;
-    var index = 0;
-    var donnee_num = (index+1)+"/"+liste_image.length;
+    //setDonnee_num((index+1)+"/"+liste_image.length);
     function suivant() {
-        if (index < liste_image.length-1) {
-            index += 1;
+        if (index < liste_image.length - 1) {
+            setIndex(index + 1);
         } else {
-            index = 0;
+            setIndex(0);
         }
-        donnee_num = (index + 1) + "/" + liste_image.length;
-        aff_num_img.textContent = donnee_num;
-        aff_img.src = liste_image[index]; 
+        setDonnee_num((index + 1) + "/" + liste_image.length);
+        //aff_num_img.textContent = donnee_num;
+        //aff_img.src = liste_image[index]; 
+        setAff_img(liste_image[index])
+        console.log(liste_image[index]);
     }
 
     function precedent() {
         if (index == 0) {
-            index = liste_image.length-1;
+            setIndex(liste_image.length - 1);
         } else {
-            index -= 1;
+            setIndex(index - 1);
         }
-        donnee_num = (index + 1) + "/" + liste_image.length;
-        aff_num_img.textContent = donnee_num;
-        aff_img.src = liste_image[index];
+        setDonnee_num((index + 1) + "/" + liste_image.length);
+        //aff_num_img.textContent = donnee_num;
+        //aff_img.src = liste_image[index];
+        setAff_img(liste_image[index])
     }
 
-   
+
 
 
     return (
         <div className="carrousel_image">
             <button id="precedent" onClick={precedent} >{"<"}</button>
-            <img id="img_actuel" src={liste[0]} />
+            <img id="img_actuel" src={aff_img} />
             <p id="num_img">{donnee_num}</p>
             <button id="suivant" onClick={suivant}>{">"}</button>
         </div>
-        )
+    )
 }
